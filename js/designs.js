027b59cc6e6d20898227742d5e1f215a4a478d51
Sibabalwe-Qamata <prof.qamata@gmail.com>
2018-06-26 20:40:24 +0200

    added the jquery library to the design.js file

--- js/designs.js
+++ js/designs.js
@@ -1,7 +1,20 @@
+//Declaring strict mode so that block-scope declarations can be used.
+"use strict";
 // Select color input
+let color;
 // Select size input
+let height;
+let width;
 
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event){
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWeight').val();
+
+    //Call the makeGrid function and pass the height, width.
+    makeGrid(height, width);
+})
 
 function makeGrid() {
 
